acf154fa83d376bcbf13df4f445a018cc31bbf4b
irene-nakabugo <irenenakabugo0@gmail.com>
2024-07-24 11:06:27 +0300

    connecting to MONGO DB

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require ('express');
+const mongoose = require('mongoose');
 
 const app = express();
-const port = 4000
+const port = 5000
 
 app.get('/', (req, res) =>{
   res.send('We are the Code Queens')
 })
-app.listen(port)
+
+//CONNECT TO THE DATABASE
+mongoose.connect("mongodb+srv://CodeQueen:CodeQueen@cluster0.crbjiye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  {useNewUrlParser:true}
+  // parsing error
+).then(
+  ()=>{console.log('Connected to the DB')}
+
+).catch(err=>{
+      console.log('Error connecting to the DB', err)
+})
 
 
-// function student (req, res){
-//console.log(req+res)
-//}
-
-// const students = (req, res)=>{ console.log (req+res)}
-
-//(req,res)=>{ console.log(req+res)}
+app.listen(port, ()=>{console.log(`Connected on port ${port}`)})
