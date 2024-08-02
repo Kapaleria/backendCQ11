61bd6fb95e3bd4b4a3956326e317dfa5657eda3c
irene-nakabugo <irenenakabugo0@gmail.com>
2024-08-02 09:49:37 +0300

    create routes

--- app.js
+++ app.js
@@ -2,23 +2,25 @@ const express = require ('express');
 const mongoose = require('mongoose');
 require('dotenv/config')
 
-const app = express(); //creating an express app
-// const port = 5000
+//importing the routes
+const routes = require('./routes')
 
-app.get('/', (req, res) =>{
+const app = express(); //creating an express app
+
+//MIDDLEWARE
+app.use('/api',routes)
+
+// app.use(express.json())
+
+app.get('/', (req, res) =>{ 
   res.send('We are the Code Queens')
+  
 })
 
 //CONNECT TO THE DATABASE
-mongoose.connect(process.env.MONGODBLINK,
-  {useNewUrlParser:true}
-  // parsing error
-).then(
-  ()=>{console.log('Connected to the DB')}
-
-).catch(err=>{
-      console.log('Error connecting to the DB', err)
-})
+mongoose.connect(process.env.MONGODBLINK)
+.then(()=>{console.log('Connected to the DB')})
+.catch(err=>{console.log('Error connecting to the DB', err)})
 
 
 app.listen(process.env.PORT, ()=>{console.log(`Connected on port ${process.env.PORT}`)})
