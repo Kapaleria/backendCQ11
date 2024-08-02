d5e044db02fd3fd1a0ca9d5244e2987b9230e8c1
irene-nakabugo <irenenakabugo0@gmail.com>
2024-08-02 15:25:53 +0300

    routers

--- routes.js
+++ routes.js
@@ -3,9 +3,19 @@ const express = require('express')
 const router = express.Router()
 
             //CRUD
-//GET METHOD
+//get method (all posts)
 router.get('/posts', (req,res)=>{
-    res.send('Posts page')
+    // res.send('Posts page')
+
+    res.json(
+        {msg:'GET all posts'}
+    )
+})
+
+router.get('/posts/:id', (req,res)=>{
+    res.json(
+        {msg:'GET a specific post'}
+    )
 })
 
 module.exports= router
